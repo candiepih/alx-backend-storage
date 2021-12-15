8e52481c890c6ea311e274dff31b4cfd34e13a36
Candiepih <mutheeal.am@gmail.com>
2021-12-15 21:08:33 +0300

    Handled formatting of string in replay method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -124,7 +124,8 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+    def get(self, key: str, fn: Optional[Callable] = None)\
+            -> Union[str, bytes, int, float, None]:
         """
         Get data from redis cache
         """
