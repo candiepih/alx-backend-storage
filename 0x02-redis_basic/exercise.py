b3e88dbe51d09bdd48abcf915ae51b6b43b5d5c2
Candiepih <mutheeal.am@gmail.com>
2021-12-15 21:04:25 +0300

    Handled formatting of string in replay method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -85,8 +85,8 @@ def replay(method: Callable) -> None:
     """
     name = method.__qualname__
     cache = redis.Redis()
-    called = cache.get(name)
-    print("{} was called {} times:".format(name, called.decode('utf-8')))
+    calls = cache.get(name).decode("utf-8")
+    print("{} was called {} times:".format(name, calls))
     inputs = cache.lrange(name + ":inputs", 0, -1)
     outputs = cache.lrange(name + ":outputs", 0, -1)
     for i, o in zip(inputs, outputs):
